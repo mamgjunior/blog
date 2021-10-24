873b0b93cd3453bf27a119a7c7a132526c8ce3b0
mamgjunior <mamgjunior@hotmail.com>
2021-10-24 17:58:48 -0300

    Criando os comentarios do blog e implementando as tags

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,7 +1,17 @@
 from django import forms
+from .models import Comment
 
+
+# Criando o formulário apenas usando o form do django
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
+
+
+# Criando um formulário dinamicamente usando a classe especifica e ModelForm do django
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
